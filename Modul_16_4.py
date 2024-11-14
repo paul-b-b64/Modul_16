2731a0935e5234a1a38e0d0671cc4ed41bb30bec
paul-b-b64 <belousko@inbox.ru>
2024-11-14 14:14:04 +0300

    Modul 16_4 id по длине списка (не так надо)

--- Modul_16_4.py
+++ Modul_16_4.py
@@ -6,10 +6,12 @@ app = FastAPI()
 
 users = []
 
+
 class User(BaseModel):
     id: int = None
     username: str
     age: int
+
 
 @app.get("/users")
 async def get_message() -> list:
@@ -22,20 +24,24 @@ async def create_user(user: User, username: str, age: int):
     user.username = username
     user.age = age
     users.append(user)
-    return f"User {user.username} registered"
+    return user
+
 
 @app.put("/user/{user_id}/{username}/{age}")
-async def update_user(user_id: str, username: str, age: int):
+async def update_user(user_id: int, username: str, age: int):
+    try:
+        edit_user = users[user_id - 1]
+        edit_user.username = username
+        edit_user.age = age
+        return edit_user
+    except IndexError:
+        raise HTTPException(status_code=404, detail="User was not found")
 
-#     try:
-#         edit_message = messages_db[message_id]
-#         edit_message.text = message
-#         return f"Message updated!"
-#     except IndexError:
-#         raise HTTPException(status_code=404, detail="Message not found")
 
-#
-# @app.delete("/user/{user_id}")
-# async def delete_user(user_id: str) -> str:
-#     users.pop(user_id)
-#     return f"The user {user_id} has been deleted"
+@app.delete("/user/{user_id}")
+async def delete_user(user_id: int):
+    try:
+        popped_user = users.pop(user_id - 1)
+        return popped_user
+    except:
+        raise HTTPException(status_code=404, detail="User was not found")
