26b3ed31d3870df6681261c9b1ba54ca922a4d68
paul-b-b64 <belousko@inbox.ru>
2024-11-14 12:23:27 +0300

    Modul 16_4 post рабочий

--- Modul_16_4.py
+++ Modul_16_4.py
@@ -12,27 +12,28 @@ class User(BaseModel):
     age: int
 
 @app.get("/users")
-def get_message() -> list:
+async def get_message() -> list:
     return users
 
 
 @app.post("/user/{username}/{age}")
-def create_user(user: User, username: str, age: int):
-    user.id = len(users)
+async def create_user(user: User, username: str, age: int):
+    user.id = len(users) + 1
     user.username = username
     user.age = age
     users.append(user)
-    return f"{users[user.id]} registered"
+    return f"User {user.username} registered"
 
-# @app.put("/user/{user_id}/{username}/{age}")
-# async def update_user(user_id: str, username: str, age: int):
+@app.put("/user/{user_id}/{username}/{age}")
+async def update_user(user_id: str, username: str, age: int):
+
 #     try:
 #         edit_message = messages_db[message_id]
 #         edit_message.text = message
 #         return f"Message updated!"
 #     except IndexError:
 #         raise HTTPException(status_code=404, detail="Message not found")
-#
+
 #
 # @app.delete("/user/{user_id}")
 # async def delete_user(user_id: str) -> str:
