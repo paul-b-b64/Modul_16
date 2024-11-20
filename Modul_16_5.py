ddcb5f03294c78f4fb8e8b78c9534cf0273b991b
paul-b-b64 <belousko@inbox.ru>
2024-11-20 15:01:56 +0300

    Pack app

---
+++ Modul_16_5.py
@@ -0,0 +1,69 @@
+from fastapi import FastAPI, status, Body, HTTPException, Request, Form
+from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+from typing import List
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+users = []
+
+
+class User(BaseModel):
+    id: int = None
+    username: str
+    age: int
+
+
+@app.get("/")
+def get_all_users(request: Request):
+    return templates.TemplateResponse("users.html", {"request": request, "user": users})
+
+
+@app.get("/user/{user_id}")
+def get_users(request: Request, user_id: int):
+    try:
+        index = None
+        for i in range(len(users)):
+            if users[i].id == user_id:
+                index = i
+        return templates.TemplateResponse("users.html", {"request": request, "user": users[index]})
+    except IndexError:
+        raise HTTPException(status_code=404, detail="User was not found")
+
+@app.post("/user/{username}/{age}")
+def create_user(user: User, username: str, age: int):
+    user.id = max([x.id for x in users], default=0) + 1
+    user.username = username
+    user.age = age
+    users.append(user)
+    return user
+
+
+@app.put("/user/{user_id}/{username}/{age}")
+def update_user(user_id: int, username: str, age: int):
+    try:
+        index = None
+        for i in range(len(users)):
+            if users[i].id == user_id:
+                index = i
+        edit_user = users[index]
+        edit_user.username = username
+        edit_user.age = age
+        return edit_user
+    except TypeError:
+        raise HTTPException(status_code=404, detail="User was not found")
+
+
+@app.delete("/user/{user_id}")
+def delete_user(user_id: int):
+    try:
+        index = None
+        for i in range(len(users)):
+            if users[i].id == user_id:
+                index = i
+        popped_user = users.pop(index)
+        return popped_user
+    except TypeError:
+        raise HTTPException(status_code=404, detail="User was not found")
