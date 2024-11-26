e9eb69c660566c5ae7112030470afa3903b16f00
paul-b-b64 <belousko@inbox.ru>
2024-11-26 16:33:18 +0300

    16_5

--- Modul_16_5.py
+++ Modul_16_5.py
@@ -4,10 +4,11 @@ from pydantic import BaseModel
 from typing import List
 from fastapi.templating import Jinja2Templates
 
-app = FastAPI()
+app = FastAPI(debug=True)
 templates = Jinja2Templates(directory="templates")
 
-users = []
+
+# users = []
 
 
 class User(BaseModel):
@@ -16,21 +17,29 @@ class User(BaseModel):
     age: int
 
 
-@app.get("/")
-def get_all_users(request: Request):
-    return templates.TemplateResponse("users.html", {"request": request, "user": users})
+users: List[User] = [
+    User(id=1, username="UrbanUser", age=24),
+    User(id=2, username="UrbanTest", age=22),
+    User(id=3, username="Capybara", age=60)
+]
 
 
-@app.get("/user/{user_id}")
-def get_users(request: Request, user_id: int):
+@app.get("/", response_class=HTMLResponse)
+async def get_all_users(request: Request):
+    return templates.TemplateResponse('users.html', {"request": request, "users": users})
+
+
+@app.get("/user/{user_id}", response_class=HTMLResponse)
+async def get_users(request: Request, user_id: int):
     try:
         index = None
         for i in range(len(users)):
             if users[i].id == user_id:
                 index = i
-        return templates.TemplateResponse("users.html", {"request": request, "user": users[index]})
+        return templates.TemplateResponse("users.html", {"request": request, "users": users[index]})
     except IndexError:
         raise HTTPException(status_code=404, detail="User was not found")
+
 
 @app.post("/user/{username}/{age}")
 def create_user(user: User, username: str, age: int):
